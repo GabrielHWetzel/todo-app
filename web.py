64db5d477e919bb198881597ed4344533d9a22c2
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-12 11:18:51 -0300

    Web app basics working

--- web.py
+++ web.py
@@ -3,9 +3,20 @@ import functions
 
 todos = functions.get_file()
 
+
+def add_todo():
+    todos.append(st.session_state["new_todo"].capitalize())
+    functions.set_file(todos)
+
+
 st.title("List of to-dos")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_file(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
